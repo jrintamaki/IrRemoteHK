d633ee0f250710e2b4b8a93ea784a7eae5217cde
Julius <julius.rintamaki@gmail.com>
2018-01-17 15:51:25 +0200

    Testing reading values from chromecast and speakers

--- IrRemoteHK.ino
+++ IrRemoteHK.ino
@@ -1,12 +1,27 @@
 #include <IRremote.h>
 #include "HK3480NEC.h" //List of commands available
 IRsend irsend;
+const int speakerInPin = A0;
 
-void setup()
-{
+int speakerValue = 0;
+int tempValue =0;
+void setup(){
+  Serial.begin(9600);
+  
 }
 
 void loop() {
-  irsend.sendNEC(VOL_UP, 32); //Sends command to receiver
-  delay(5000); //5 second delay between next command
+  //speakerValue =  analogRead(speakerInPin);
+  //Serial.print("System ok\n");
+  tempValue = 0;
+  for (int i=1; i<10;i++){
+    tempValue += analogRead(speakerInPin);
+  }
+  speakerValue = tempValue / 10;
+  Serial.print("Speaker = ");
+  Serial.print(speakerValue);
+  Serial.print("\n");
+  delay(100);
+  //irsend.sendNEC(VOL_UP, 32); //Sends command to receiver
+  //delay(5000); //5 second delay between next command
 }
