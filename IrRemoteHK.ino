fc02d083345b58f529421a27edbaaf1b106de947
Julius <julius.rintamaki@gmail.com>
2018-01-17 16:46:34 +0200

    Now turns receiver off if no signal outof  speakers

--- IrRemoteHK.ino
+++ IrRemoteHK.ino
@@ -2,11 +2,32 @@
 #include "HK3480NEC.h" //List of commands available
 IRsend irsend;
 
+const int speakerInPin = A0;
+
+int speakerValue = 0;
+int speakerOffTime = 20000;
+int tempValue = 0;
+
 void setup()
 {
+  Serial.begin(9600);
 }
 
 void loop() {
-  irsend.sendNEC(VOL_UP, 32); //Sends command to receiver
-  delay(5000); //5 second delay between next command
+  if (0 == analogRead(speakerInPin)) {
+    speakerValue++;
+    Serial.print(speakerValue);
+    Serial.print("\n");
+
+    if (speakerValue > speakerOffTime) {
+      irsend.sendNEC(POWER_OFF, 32);
+      speakerValue = 0;
+    }
+
+  }
+  else
+      speakerValue = 0;
+  //delay(50);
+  //irsend.sendNEC(VOL_UP, 32); //Sends command to receiver
+  //delay(5000); //5 second delay between next command
 }
