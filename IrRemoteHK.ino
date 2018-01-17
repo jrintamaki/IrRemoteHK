51a1f50380be4322b4887491e80f3ec10bac5b32
Julius <julius.rintamaki@gmail.com>
2018-01-17 17:36:11 +0200

    Now switches source to chromecast if it is playing

--- IrRemoteHK.ino
+++ IrRemoteHK.ino
@@ -5,15 +5,13 @@ IRsend irsend;
 const int speakerInPin = A0;
 
 int speakerValue = 0;
-int speakerOffTime = 20000;
-int tempValue = 0;
+int speakerOffTime = 2000;
+//int tempValue = 0;
 
-void setup()
-{
-  Serial.begin(9600);
-}
-
-void loop() {
+void checkSpeakers() { 
+/*Checks are spekers playing sound.  
+After enough consencutive 0 signals will turn receiver off.
+*/
   if (0 == analogRead(speakerInPin)) {
     speakerValue++;
     Serial.print(speakerValue);
@@ -26,7 +24,30 @@ void loop() {
 
   }
   else
-      speakerValue = 0;
+    speakerValue = 0;
+}
+void checkInput(int pin, long source){
+  int tempValue = 0;
+  for (int i=1; i<100;i++){
+    tempValue += analogRead(pin);
+    
+  }
+  Serial.print(tempValue);
+  Serial.print("\n");
+  if (tempValue / 100> 1){
+    Serial.print(source);
+    irsend.sendNEC(source, 32);
+  }
+}
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop() {
+  checkInput(A1, VIDEO2);
+  checkSpeakers();
+  
   //delay(50);
   //irsend.sendNEC(VOL_UP, 32); //Sends command to receiver
   //delay(5000); //5 second delay between next command
